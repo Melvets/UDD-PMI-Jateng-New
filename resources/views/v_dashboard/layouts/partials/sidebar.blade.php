a68ea451793ce863dad9d24a14b4c1699f22d2ef
Camela Devs <camelaputri1@gmail.com>
2024-01-18 14:03:58 +0700

    Update Login page

--- resources/views/v_dashboard/layouts/partials/sidebar.blade.php
+++ resources/views/v_dashboard/layouts/partials/sidebar.blade.php
@@ -85,3 +85,22 @@
     </span>
   </a>
 </li>
+
+@can('admin')
+
+<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-2 mb-0 text-uppercase" style="color: gray">
+    <span>Administrator</span>
+</h6>
+
+<li class="side-link_ nav-item">
+    <a class="nav-link {{ Request::is('dashboard/jadwalmu*') ? 'active-side' : '' }}" href="/dashboard/jadwalmu">
+      <span class="nav-link-icon d-md-none d-lg-inline-block"><!-- Download SVG icon from http://tabler-icons.io/i/home -->
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-calendar-stats" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M11.795 21h-6.795a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v4" /><path d="M18 14v4h4" /><path d="M18 18m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" /><path d="M15 3v4" /><path d="M7 3v4" /><path d="M3 11h16" /></svg>
+      </span>
+      <span class=" side-link-title_ nav-link-title">
+        Buat Akun
+      </span>
+    </a>
+  </li>
+
+@endcan
--- resources/views/v_login/index.blade.php
+++ resources/views/v_login/index.blade.php
@@ -14,63 +14,54 @@
                 class="img-fluid" alt="Sample image">
             </div>
             <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                
+
                 <div class="card" style="width: 25rem;">
                     <div class="card-body">
                       <h5 class="card-title fw-bold text-center mt-3 mb-0 pb-0" style="color: #e60000; font-size: 2.5em;">LOGIN</h5>
                       <h6 class="card-subtitle mb-4 text-body-secondary text-center" style="font-size: 0.8em">Login to your account</h6>
-                        
+
+                        @if(session()->has('loginError'))
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                {{ session('loginError') }}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        @endif
+
                         <form action="/login" method="POST">
 
                             @csrf
-            
+
                             <div class="form-floating mb-3">
-                                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value="{{ old('email') }}" autofocus required>
+                                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" value="{{ old('email') }}" autofocus required>
                                 <label for="email" style="font-size: 0.9em">Email address</label>
-            
+
                                 @error('email')
                                     <div class="invalid-feedback mb-2">
                                         {{ $message }}
                                     </div>
                                 @enderror
-            
+
                             </div>
                             <div class="form-floating mb-3">
-                                <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+                                <input type="password" name="password" class="form-control" id="password" required>
                                 <label for="password" style="font-size: 0.9em">Password</label>
-            
+
                                 @error('password')
                                     <div class="invalid-feedback mb-2">
                                         {{ $message }}
                                     </div>
-                                @enderror 
+                                @enderror
                             </div>
-                        
-                            <button class="btn text-white w-100 pt-2" type="submit" style="background-color: #cc0000">Login</button>
-                        </form>
 
-                        <small class="d-block text-center mt-3 mb-5">Not registed? <a href="/register">Register Now!</a></small>
+                            <button class="btn text-white w-100 pt-2 mb-5" type="submit" style="background-color: #cc0000">Login</button>
+                        </form>
 
                     </div>
                 </div>
-                
+
             </div>
         </div>
         </div>
     </section>
-
-    @if(session()->has('success'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                {{ session('success') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-    @endif
-
-    @if(session()->has('loginError'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            {{ session('loginError') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-    @endif
 
 @endsection
